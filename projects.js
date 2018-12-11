f5d816677194c929bf768a314014592bc4d95c39
Bennett Maciorowski <bmacio2@uic.edu>
2018-12-11 21:42:10 +0000

    updated project Listing

--- projects.js
+++ projects.js
@@ -1,15 +1,24 @@
 var projs = [{
     name_: "IT202 Final Project",
-    repoLink: "../IT202-Final-Project/",
+    repoLink: "../IT202-Final-Project",
     description: "My final project for IT202"
 }, {
     name_: "Linux Users Group Website",
     repoLink: "https://github.com/lugatuic/lug.cs.uic.edu-2018",
-    description: "A new website for LUG"
+    description: "A new website for LUG @ UIC. Most Recent Branch is under the development branch. Master is reserved for releases"
 },{
     name_: "Why Are You Running",
     repoLink: "../whyAreYouRunning",
     description: "My experiments with speech stuff"
+},{
+    name_: "Google Login Authentication",
+    repoLink: "../IT202-Lab-14",
+    description: "My experimenting with google authentication"
+},{
+    
+    name_: "IT 202 Week5 Lab",
+    repoLink: "../CS202-week5-lab",
+    description: "Queries City of Chicago dataportal for data on car and informs you on whether or not you've been towed"
 }
 ];
 $(document).ready(function() {
